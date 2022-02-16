79630f2a02a8f4eccdebe58d0551f9934b0d8a80
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-16 23:57:36 +0800

    Cleaned code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const displayCurrent = document.querySelector(".current-results");
 const displayPast = document.querySelector(".past-results");
 
-let total = 0;
-let operand = '';
+let i = 0;
 let number = '';
-let numbers = [];
+let arrayNumbers = [];
+let arrayOperations = [];
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
@@ -14,178 +14,73 @@ buttons.forEach(button => {
             number = `${number}${buttonClicked}`
             displayCurrent.textContent = number;
         }
-
-        else if (total == "TO INFINITY AND BEYOND") {
-            displayPast.textContent = '';
-            displayCurrent.textContent = "TO INFINITY AND BEYOND";
+        else if (buttonClicked == 'c') {
             number = '';
-            numbers = [];
-            total = 0;
-            operand = '';
-        }
-
-        else {
-            if (buttonClicked === 'c') {
-                displayPast.textContent = '';
-                displayCurrent.textContent = 0;
-                number = '';
-                numbers = [];
-                total = 0;
-                operand = '';
-            }
-
-            else if (buttonClicked === '+') {
-                numbers.push(Number(number));
-                if (operand == '') {
-                    numbers.push(0);
-                } else if (operand == '~') {
-                    number = `${number}${buttonClicked}`
-                    operand = '+';
-                }
-                total = operator(operand, numbers, number);
-                displayPast.textContent = number;
-                displayCurrent.textContent = total;
-                number = '';
-                numbers[0] = Number(total);
-                numbers.pop()
-                operand = '+';
-
-            }
-
-            else if (buttonClicked === '-') {
-                numbers.push(Number(number));
-                if (operand == '') {
-                    numbers.push(0);
-                } else if (operand == '~') {
-                    number = `${number}${buttonClicked}`
-                    operand = '-';
-                }
-                total = operator(operand, numbers, number);
-                displayPast.textContent = number;
-                displayCurrent.textContent = total;
-                number = '';
-                numbers[0] = Number(total);
-                numbers.pop()
-                operand = '-';
-            }
-
-            else if (buttonClicked === 'x') {
-                numbers.push(Number(number));
-                if (operand == '') {
-                    numbers.push(1);
-                } else if (operand == '~') {
-                    number = `${number}${buttonClicked}`
-                    operand = 'x';
-                }
-                total = operator(operand, numbers, number);
-                displayPast.textContent = number;
-                displayCurrent.textContent = total;
-                number = '';
-                numbers[0] = Number(total);
-                numbers.pop()
-                operand = 'x';
-            }
-
-            else if (buttonClicked === '÷') {
-                numbers.push(Number(number));
-                if (operand == '') {
-                    numbers.push(1);
-                } else if (operand == '~') {
-                    number = `${number}${buttonClicked}`
-                    operand = '÷';
-                }
-                total = operator(operand, numbers, number);
-                displayPast.textContent = number;
-                displayCurrent.textContent = total;
-                if (total == "TO INFINITY AND BEYOND") {
-                    displayPast.textContent = '';
-                    displayCurrent.textContent = "TO INFINITY AND BEYOND";
-                    number = '';
-                    numbers = [];
-                    total = 0;
-                    operand = '';
-                }
-                number = '';
-                numbers[0] = Number(total);
-                numbers.pop()
-                operand = '÷';
-            }
-
-            else if (buttonClicked === '%') {
-                numbers.push(Number(number));
-                total = percent(numbers[0]);
-                displayPast.textContent = `${number}%`
-                displayCurrent.textContent = total;
-                numbers.pop();
-                operand = '%'
-            }
-
-            else if (buttonClicked === '!') {
-                numbers.push(Number(number));
-                total = factorial(numbers[0]);
-                displayPast.textContent = `${number}!`
-                displayCurrent.textContent = total;
-                numbers.pop();
-                operand = '!'
-            }
-
-            else if (buttonClicked === '=') {
-                if (operand == '~') number = total;
-                numbers.push(Number(number));
-                total = operator(operand, numbers, number);
-                console.log(total);
-                if (total == "TO INFINITY AND BEYOND") {
-                    displayPast.textContent = '';
-                    displayCurrent.textContent = "TO INFINITY AND BEYOND";
-                    number = '';
-                    numbers = [];
-                    total = 0;
-                    operand = '';
-                }
-                displayPast.textContent = number;
-                displayCurrent.textContent = total;
-                operand = '~';
-                if (numbers.length != 1) number = '';
-                else numbers.push(number);
-                numbers[0] = Number(total);
-                numbers.pop();
-
-            }
-
-            else if (buttonClicked === '+/-') {
-                number = `-${number}`;
+            displayPast.textContent = '';
+            displayCurrent.textContent = 0;
+            arrayNumbers = [];
+            arrayOperations = [];
+            i = 0;
+        } else if (buttonClicked == '.') {
+            if (!(number.search('.'))) {
+                number = `${number}.`;
                 displayCurrent.textContent = number;
             }
+        } else if (buttonClicked === '+/-') {
+            number = `-${number}`;
+            displayCurrent.textContent = number;
+        } else {
+            arrayOperations.push(buttonClicked);
+            arrayNumbers.push(displayCurrent.textContent);
+            number = '';
 
-            else if (buttonClicked === '.') {
-                if (number.search('.')) {
-                    number = "ERROR";
-                    number = '';
-                    numbers = [];
-                    operand = '';
-                }
-                else number = `${number}.`;
-                displayCurrent.textContent = number;
+            if (buttonClicked == '%') {
+                displayPast.textContent = `${arrayNumbers[i]} ${arrayOperations[i]}`
+                displayCurrent.textContent = percent(arrayNumbers[i]);
+                number = '';
+                arrayNumbers = [];
+                arrayOperations = [];
+                i = 0;
+            } else if (buttonClicked == '!') {
+                displayPast.textContent = `${arrayNumbers[i]} ${arrayOperations[i]}`
+                displayCurrent.textContent = factorial(arrayNumbers[i]);
+                number = '';
+                arrayNumbers = [];
+                arrayOperations = [];
+                i = 0;
+            } else if (arrayNumbers.length == 1) {
+                displayPast.textContent = `${arrayNumbers[0]} ${arrayOperations[0]}`
+                displayCurrent.textContent = arrayNumbers[0];
+            } else if (buttonClicked == '=') {
+                displayCurrent.textContent = operate(arrayNumbers, arrayOperations, i);
+                number = '';
+                arrayNumbers = [];
+                arrayOperations = [];
+                i = 0;
+            } else if (arrayNumbers.length > 1) {
+                displayPast.textContent = `${arrayNumbers[i]} ${arrayOperations[i]} ${arrayNumbers[i + 1]} =`;
+                displayCurrent.textContent = operate(arrayNumbers, arrayOperations, i);
+                i++;
             }
         }
+
     });
 });
 
-function addition(args) {
-    return parseFloat((args[0] + args[1]).toFixed(5));
+function addition(number1, number2) {
+    return parseFloat((+number1 + +number2).toFixed(5));
 }
 
-function subtraction(args) {
-    return parseFloat((args[0] - args[1]).toFixed(5));
+function subtraction(number1, number2) {
+    return parseFloat((+number1 - +number2).toFixed(5));
 }
 
-function multiplication(args) {
-    return parseFloat((args[0] * args[1]).toFixed(5));
+function multiplication(number1, number2) {
+    return parseFloat((+number1 * +number2).toFixed(5));
 }
 
-function division(args) {
-    console.log(args[1]);
-    if (args[1] != 0) return parseFloat((args[0] / args[1]).toFixed(5));
+function division(number1, number2) {
+    if (+number2 != 0) return parseFloat((+number1 / +number2).toFixed(5));
     else return "TO INFINITY AND BEYOND";
 }
 
@@ -205,10 +100,12 @@ function factorial(args) {
     }
 }
 
-function operator(operand, args, number) {
-    if (operand == '+') return addition(args);
-    else if (operand == '-') return subtraction(args);
-    else if (operand == 'x') return multiplication(args);
-    else if (operand == '÷') return division(args);
-    else return number;
+
+function operate(arrayNumbers, arrayOperations, i) {
+    if (arrayOperations[i] == '+') arrayNumbers[i + 1] = addition(arrayNumbers[i], arrayNumbers[i + 1]);
+    else if (arrayOperations[i] == '-') arrayNumbers[i + 1] = subtraction(arrayNumbers[i], arrayNumbers[i + 1]);
+    else if (arrayOperations[i] == 'x') arrayNumbers[i + 1] = multiplication(arrayNumbers[i], arrayNumbers[i + 1]);
+    else if (arrayOperations[i] == '÷') arrayNumbers[i + 1] = division(arrayNumbers[i], arrayNumbers[i + 1]);
+    else arrayNumbers[i + 1] = arrayNumbers[i + 1];
+    return arrayNumbers[i + 1];
 }
